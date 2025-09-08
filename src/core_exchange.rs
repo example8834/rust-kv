347f996d26f052e017e73282641348560501fd1f
wx <13453446455@163.com>
2025-09-08 20:43:39 +0800

    整体去除拷贝性能优化 大幅优化Frame 转换Command 性能

--- src/core_exchange.rs
+++ src/core_exchange.rs
@@ -9,36 +9,39 @@ impl TryFrom<Frame> for Command {
     type Error = KvError;
 
     fn try_from(frame: Frame) -> Result<Self, Self::Error> {
-        let frames = match frame {
+        let  frames = match frame {
             Frame::Array(frames) => frames,
             _ => return Err(ProtocolError("must be a array from frame".into())),
         };
-
-        match frames.get(0) {
-            Some(Frame::Bulk(start_str)) => {
-                let command_name = std::str::from_utf8(start_str)
+        if frames.is_empty() {
+            return Err(ProtocolError("frame is empty".into()));
+        }
+        let length = frames.len();
+        let mut iter = frames.into_iter();
+        match iter.next() {
+         Some(Frame::Bulk(start_str)) => {
+                let command_name = String::from_utf8(start_str.to_vec())
                     .map_err(|_| ProtocolError("zhuan huan yi chang ".into()))?
                     .to_uppercase();
                 match command_name.as_str() {
                     "GET" => {
-                        if frames.len() != 2 {
+                        if length != 2 {
                             return Err(ProtocolError("GET 命令需要 1 个参数".into()));
                         }
-                        let get_key = extract_bulk_string(&frames[1])?;
+                        let get_key = extract_bulk_string(iter.next())?;
                         Ok(Get { key: get_key })
                     }
                     "SET" => {
-                        if frames.len() < 3 {
+                        if length < 3 {
                             return Err(ProtocolError("frame is too short".into()));
                         }
-                        let set_key = extract_bulk_string(&frames[1])?;
-                        let set_value = extract_bulk_bytes(&frames[2])?;
+                        let set_key = extract_bulk_string(iter.next())?;
+                        let set_value = extract_bulk_bytes(iter.next())?;
                         let mut expiration: Option<Expiration> = None;
                         let mut condition: Option<SetCondition> = None;
                         let mut index = 3;
-                        let length = frames.len();
                         while index < length - 1 {
-                            let frame_str = extract_bulk_string(&frames[index])?.to_uppercase();
+                            let frame_str = extract_bulk_string(iter.next())?.to_uppercase();
                             match frame_str.as_str() {
                                 "EX" => {
                                     // EX 后面必须跟一个数字
@@ -46,7 +49,7 @@ impl TryFrom<Frame> for Command {
                                     if index >= length {
                                         return Err(ProtocolError("😧 EX 后参数传递错误".into()));
                                     }
-                                    let seconds = extract_bulk_integer(&frames[index])?;
+                                    let seconds = extract_bulk_integer(iter.next())?;
                                     expiration = Some(Expiration::EX(seconds as u64 ));
                                 }
                                 "PX" => {
@@ -55,7 +58,7 @@ impl TryFrom<Frame> for Command {
                                     if index >= length {
                                         return Err(ProtocolError("😧 PX 后参数传递错误".into()));
                                     }
-                                    let ms = extract_bulk_integer(&frames[index])?;
+                                    let ms = extract_bulk_integer(iter.next())?;
                                     expiration = Some(Expiration::PX(ms as u64));
                                 }
                                 "NX" => {
@@ -86,8 +89,8 @@ impl TryFrom<Frame> for Command {
                         })
                     }
                     "PING" => {
-                        let msg = if frames.len() > 1 {
-                            Some(extract_bulk_string(&frames[1])?)
+                        let msg = if length > 1 {
+                            Some(extract_bulk_string(iter.next())?)
                         } else {
                             None
                         };
@@ -96,8 +99,7 @@ impl TryFrom<Frame> for Command {
 
                     // 4. 所有其他不认识的命令，都匹配到这里
                     _ => {
-                        let args = frames
-                            .into_iter()
+                        let args = iter
                             .skip(1)
                             .map(|f| match f {
                                 Frame::Bulk(bytes) => Ok(bytes),
@@ -118,9 +120,9 @@ impl TryFrom<Frame> for Command {
 }
 
 /// 尝试从一个 Frame 中提取出 Bulk String 并转换为 String
-fn extract_bulk_string(frame: &Frame) -> Result<String, KvError> {
+fn extract_bulk_string(frame: Option<Frame>) -> Result<String, KvError> {
     match frame {
-        Frame::Bulk(bytes) => Ok(std::str::from_utf8(bytes)
+        Some(Frame::Bulk(bytes)) => Ok(String::from_utf8(bytes.to_vec())
             .map_err(|e| ProtocolError(e.to_string()))?
             .to_string()),
         _ => Err(ProtocolError("期望参数是批量字符串".into())),
@@ -128,14 +130,14 @@ fn extract_bulk_string(frame: &Frame) -> Result<String, KvError> {
 }
 
 /// 尝试从一个 Frame 中提取出 Bulk String 并转换为 String
-fn extract_bulk_integer(frame: &Frame) -> Result<i64, KvError> {
+fn extract_bulk_integer(frame: Option<Frame>) -> Result<i64, KvError> {
     extract_bulk_string(frame)?.parse::<i64>().map_err(|e|ProtocolError(e.to_string()))
 }
 
 /// 尝试从一个 Frame 中提取出 Bulk Bytes
-fn extract_bulk_bytes(frame: &Frame) -> Result<Bytes, KvError> {
+fn extract_bulk_bytes(frame: Option<Frame>) -> Result<Bytes, KvError> {
     match frame {
-        Frame::Bulk(bytes) => Ok(bytes.clone()),
+        Some(Frame::Bulk(bytes)) => Ok(bytes),
         _ => Err(ProtocolError("期望参数是批量字符串".into())),
     }
 }
